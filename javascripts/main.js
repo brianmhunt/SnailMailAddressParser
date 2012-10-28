a817ccd025468d4679d63fea42c3b2e01844d3bf
Brian M Hunt <brianmhunt@gmail.com>
2012-10-28 12:38:47 -0400

    Made the main page a place to give it a go

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+$(function () {
+    var $in = $("#in"),
+        $out = $("#out"),
+        smap = window.snailmailaddressparser;
+
+    $in.on("keyup", _.debounce(function () {
+        var obj;
+        
+        try {
+            obj = smap.parse($in.val());
+            $out.text(JSON.stringify(obj, null, 2));
+        } catch (err) {
+            console.log(err);
+            $out.text(err);
+        }
+    }, 250));
+
+    $in.keyup();
+});
---
+++ javascripts/snailmailaddressparser.js
@@ -0,0 +1,1 @@
+/Users/bmh/dev/SnailMailAddressParser/build/snailmailaddressparser.js
